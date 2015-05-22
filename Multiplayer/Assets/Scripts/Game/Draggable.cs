6e73659298135a236bd30f7931e025412ba74697
Simon Kok <simonkoak@gmail.com>
2015-05-22 10:46:46 +0200

    Draggable
    Draggable utility

---
+++ Multiplayer/Assets/Scripts/Game/Draggable.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections;
+
+public class Draggable : MonoBehaviour, IDragHandler, IEndDragHandler
+{
+    public bool IsUIObject;     //Object is an UI element.
+    public bool DragPhysically; //Object will be physically dragged.
+    public bool ReturnToOrigin; //Object will return to its original position after being dragged.
+    private Vector3 origin;
+
+    void Awake() {
+        IsUIObject = !gameObject.GetComponent<Collider>();
+    }
+
+    void Start()
+    {
+        origin = transform.position;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        if(DragPhysically)
+            transform.position += new Vector3(eventData.delta.x, eventData.delta.y);
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        if(DragPhysically && ReturnToOrigin)
+            transform.position = origin;
+
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+ 
+        if (Physics.Raycast(ray, out hit, Mathf.Infinity))
+        {
+            Draggable draggable = hit.collider.gameObject.GetComponent<Draggable>();
+            if (draggable)
+                DraggableInteraction.SINGLETON.HandleDraggableInteraction(this, draggable);
+        }
+    }
+}
---
+++ Multiplayer/Assets/Scripts/Game/DraggableInteraction.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DraggableInteraction : MonoBehaviour {
+    public static DraggableInteraction SINGLETON;
+
+    void Awake()
+    {
+        SINGLETON = this;
+    }
+
+    public void HandleDraggableInteraction(Draggable a, Draggable b)
+    {
+        if (!a || !b) //One or more objects are null.
+            return;
+
+        if (a.IsUIObject && b.IsUIObject) //Both objects are UI objects.
+            return;
+
+        Debug.Log("Dragged " + a.gameObject.name + " onto " + b.gameObject.name);
+
+        if (a.IsUIObject) //Try spawning a unit.
+        {
+            Debug.Log("Spawn");
+        }
+        else //Try swapping.
+        {
+            Debug.Log("Swap");
+        }
+    }
+}
