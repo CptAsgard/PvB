675a72d2ec8c27861c5257548df15f541ebbf6eb
Simon Kok <simonkoak@gmail.com>
2015-05-31 16:40:56 +0200

    End screen UI

---
+++ Multiplayer/Assets/Scripts/UI/EndScreenUI.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EndScreenUI : MonoBehaviour, MessageReceiver<EndGame> {
+
+    void Awake()
+    {
+        this.Subscribe<EndGame>(Messenger.Bus);
+    }
+
+    public void HandleMessage(EndGame msg)
+    {
+        iTween.MoveTo(gameObject, gameObject.transform.position + new Vector3(0, 800, 0), 2);
+    }
+
+    public void MainMenuButtonClicked()
+    {
+        Application.LoadLevel(0);
+    }
+}
